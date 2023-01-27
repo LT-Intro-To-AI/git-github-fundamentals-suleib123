ad94c81d6633e65e105792084b5a2285c3608ca3
suleib123 <suleimanbutt94@gmail.com>
2023-01-27 10:30:24 -0600

    Adding number guess game after greeting

--- greeting.py
+++ greeting.py
@@ -1,4 +1,13 @@
+from random import randint
+
 f_name = input("What is your first name? ") 
 l_name = input("What is your last name? ")
 
 print("Hello", f_name, l_name+"!")
+
+guess = input("Guess a number 1-10: ")
+
+if guess != randint(1, 10):
+    print("Wrong")
+else:
+    print("Correct")
