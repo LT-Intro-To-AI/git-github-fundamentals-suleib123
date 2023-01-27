b21355b8bba8ef75cb606e998c9e1a97773503b3
suleib123 <suleimanbutt94@gmail.com>
2023-01-27 10:19:38 -0600

    Collect more information from user and greet with full name.

--- greeting.py
+++ greeting.py
@@ -1,3 +1,4 @@
-name = input("What is your name? ")
+f_name = input("What is your first name? ")
+l_name = input("What is your last name? ")
 
-print("Hello", name+"!")
+print("Hello", f_name, l_name+"!")
